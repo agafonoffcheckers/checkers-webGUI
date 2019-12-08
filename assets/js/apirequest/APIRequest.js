55f312e7060050d880795afe4bd5370abd75123f
Agafonov Andrew <pentri4@gmail.com>
2019-12-08 15:27:17 +0700

    An API for requests.
    Includes methods: GET, POST.
    
    All data in JSON format.

---
+++ assets/js/apirequest/APIRequest.js
@@ -0,0 +1,34 @@
+export class APIRequest {
+    static async GET(url) {
+        let props = {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        };
+        const response = await fetch(url, props);
+        if(!response.ok) {
+            throw new Error(`Can't GET ${url}, with status: ${response.status}`)
+        }
+
+        return await response.json();
+    }
+    static async POST(url = '', data = {}) {
+        let props = {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+
+        const response = await fetch(url, props);
+        if(!response.ok) {
+            throw new Error(`Can't POST ${url}, with status: ${response.status}`)
+        }
+        return await response.json();
+    }
+}
